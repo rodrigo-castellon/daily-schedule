d00effe4a9b306205b2278e0a1c6c8b2c1caff69
Rodrigo Castellon <rodrigo.castellon@pinecrest.edu>
2017-12-12 23:25:00 -0500

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,13 @@
+# This is where your personal credentials go.
+# If you think this is unsafe, you can go the extra mile and after it is compiled, delete secrets.py and leave secrets.pyc, 
+# leaving it completely obfuscated to the casual "shoulder surfer"
+
+username = "firstname.lastname"
+password = "open-sesame"
+webaddress = "http://elearning.pinecrest.edu"
+ftp_username = "username"
+ftp_passwd = "open-sesame"
+gh_username = "username"
+gh_password = "open-sesame"
+gdocs_email = "firstname.lastname@pinecrest.edu"
+gdocs_password = "open-sesame"
